cd12dd8d27d90de7565ff1a120f2db97b16687cb
unknown <Lilkhachatyan>
2020-07-19 06:11:41 +0400

    first build

---
+++ precache-manifest.c5e6442ee8f49122ca4fd69cdde62f8a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d68d17b214b406bba9c707be9718feb7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d3558e22e6e787df7be",
+    "url": "/static/css/2.ee545042.chunk.css"
+  },
+  {
+    "revision": "01cb0c662400a5c8bb11",
+    "url": "/static/css/main.e678c733.chunk.css"
+  },
+  {
+    "revision": "1d3558e22e6e787df7be",
+    "url": "/static/js/2.623a2809.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/static/js/2.623a2809.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01cb0c662400a5c8bb11",
+    "url": "/static/js/main.687e584c.chunk.js"
+  },
+  {
+    "revision": "8b8f8712f7998908b0a9",
+    "url": "/static/js/runtime-main.8c44053c.js"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/static/media/notification.c0d3c94c.eot"
+  }
+]);
